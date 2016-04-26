5e5a7bde5571e396d7c34e8b86dad9eb8446a364
SebastianStachowiak <soiskbi@gmail.com>
2016-04-26 10:24:37 +0200

    PIN

---
+++ PIN/scripts/JS.js
@@ -0,0 +1,24 @@
+.button {
+    background-color: #000000;
+    border-radius: 15px;
+    color: white;
+	width: 210px;
+    padding: 30px;
+    font-size: 20px;
+    border: none;
+    cursor: pointer;
+}
+.button:hover {
+    background-color: #202020;
+}
+.home {
+    background-color: #000000;
+    color: white;
+    padding: 16px;
+    font-size: 40px;
+    border: none;
+    cursor: pointer;
+}
+.home:hover {
+    background-color: #202020;
+}
